39ff915b2ec4f164258b15bce28541c9fb914d6e
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-27 09:42:20 +0300

    modified anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        for w in words:
+            if sorted(self.word) == sorted(w):
+                anagrams.append(w)
+        return anagrams
+
+listen = Anagram("listen")
+anagrams = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(anagrams)
